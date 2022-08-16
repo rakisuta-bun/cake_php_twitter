81254a97224fe33ff5f000fd5ac0b34a4d630947
rakisuta_bun <37924639+rakisuta-bun@users.noreply.github.com>
2022-08-16 15:03:30 +0900

    アップロードできた〜

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -28,6 +28,23 @@ class ProfileController extends AppController
         $user = $this->Authentication->getIdentity();
         /** @var TweetsTable $tweetsTable */
         $this->set(['user' => $user]);
+        if ($this->request->is('post')) {
+            $file = $this->request->getData("file");
+            if ($file != "undefind" && $file->getClientMediaType() != '') {
+                $type = $file->getClientMediaType();
+                $extension = '';
+                switch ($type) {
+                    case 'image/jepg':
+                        $extension = '.jpg';
+                        break;
+                    case 'image/png':
+                        $extension = '.png';
+                }
+                $land = Text::uuid();
+                $filePath = WWW_ROOT . "/img/" . $land . $extension;
+                $file->moveTo($filePath);
+            }
+        }
     }
 
     public function file()
